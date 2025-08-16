24ced38f7157606eed571177092ade33a91de962
SPodgainova <podgainovasvetlana@yandex.ru>
2025-08-16 14:00:33 +0300

    feat/validation for name

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,9 +2,9 @@ import Button from "../Button/Button";
 import FloatInput from "../FloatInput/FloatInput";
 
 import styles from "./styles.module.scss";
-import type { IItemsForm} from "./type";
+import type { IItemsForm } from "./type";
 
-import { Controller, useForm, type SubmitHandler} from "react-hook-form"
+import { Controller, useForm, type SubmitHandler } from "react-hook-form"
 
 // to Do
 // для описания и заметок поменять инпут на textarea
@@ -20,67 +20,64 @@ import { Controller, useForm, type SubmitHandler} from "react-hook-form"
 
 export const Form = () => {
 
-  const {control, handleSubmit, resetField} = useForm<IItemsForm>({defaultValues: {
-    link: "",
-    image: "",
-    name: "",
-    description: "",
-    notes: "",
-  }})
+  const { control, handleSubmit, resetField } = useForm<IItemsForm>({
+    defaultValues: {
+      link: "",
+      image: "",
+      name: "",
+      description: "",
+      notes: "",
+    }
+  })
 
-  const submit : SubmitHandler<IItemsForm> = (data) => console.log(data);
+  const submit: SubmitHandler<IItemsForm> = (data) => console.log(data);
 
   return (
     <form onSubmit={handleSubmit(submit)} className={styles.form}>
-     <Controller name="link" control={control} rules={{validate: (value) => {
-      if(!value) return true;
-      try {
-        new URL(value);
-        return true
-      }
-      catch {
-        return "Введите корректный URL"
-      }
-     }}} render={({field, fieldState }) => <FloatInput {...field} type="text" label="Ссылка" error={fieldState.error?.message} onClear={() => {
-      resetField('link')
-     }}  />}/>
+      <Controller name="link" control={control} rules={{
+        validate: (value) => {
+          if (!value) return true;
+          try {
+            new URL(value);
+            return true
+          }
+          catch {
+            return "Введите корректный URL"
+          }
+        }
+      }} render={({ field, fieldState }) => <FloatInput {...field} type="text" label="Ссылка" error={fieldState.error?.message} onClear={() => {
+        resetField('link')
+      }} />} />
 
-      {/* <div className={styles.wrapper}>
+      <div className={styles.wrapper}>
         <div className={styles.imageWrapper}>
-          <ImageUploader
+          {/* <ImageUploader
             previewUrl={getPreviewUrl()}
             onChange={handleChangeFile}
             onClick={handleImageDelete}
-          />
-          <FloatInput
+          /> */}
+          {/* <FloatInput
             name="image"
             type="url"
             value={formData.image}
             label="Ссылка на изображение"
             onChange={handleImageChange}
             onClear={() => clearField("image")}
-          />
+          /> */}
         </div>
         <div className={styles.inputsWrapper}>
-          <FloatInput
-            name="name"
-            type="text"
-            value={formData.name}
-            label="Введите название"
-            onChange={handleChange}
-            onClear={() => clearField("name")}
-          />
-          <FloatInput
+          <Controller name="name" control={control} rules={{ required: { value: true, message: "Введите минимум 1 символ" }, minLength: 1 }} render={({ field, fieldState }) => <FloatInput {...field} type="text" label="Нименование *" error={fieldState.error?.message} onClear={() => resetField("name")} />} />
+          {/* <FloatInput
             name="description"
             type="text"
             value={formData.description}
             label="Введите описание"
             onChange={handleChange}
             onClear={() => clearField("description")}
-          />
+          /> */}
         </div>
       </div>
-      <FloatInput
+      {/* <FloatInput
         name="notes"
         type="text"
         value={formData.notes}

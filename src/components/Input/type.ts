8116f45da4eb88cc87288207af50d3fa9bf3645e
SPodgainova <podgainovasvetlana@yandex.ru>
2025-08-16 19:33:37 +0300

    refactor

--- src/components/Input/type.ts
+++
@@ -1,8 +0,0 @@
-export interface IInputProps<T extends object> {
-  value: string;
-  name: keyof T;
-  type: "text" | "number" | "url";
-  placeholder?: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  required?: boolean;
-}
